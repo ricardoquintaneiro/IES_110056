faa0f1f5464f1923ccb9e53246b3bf740bcdcbb1
Ricardo Quintaneiro <97852246+ricardoquintaneiro@users.noreply.github.com>
2023-10-10 14:58:42 +0100

    Lab 2.3 Spring Initializr

---
+++ lab2/lab2_3/boot/src/test/java/ies/spring/boot/BootApplicationTests.java
@@ -0,0 +1,13 @@
+package ies.spring.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
