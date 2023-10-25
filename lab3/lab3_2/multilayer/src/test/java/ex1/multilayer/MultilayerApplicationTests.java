a742c4bfe901328958a30c75aac6bcc0c1dce8de
Ricardo Quintaneiro <97852246+ricardoquintaneiro@users.noreply.github.com>
2023-10-25 22:42:37 +0100

    Lab 3.2 incomplete

---
+++ lab3/lab3_2/multilayer/src/test/java/ex1/multilayer/MultilayerApplicationTests.java
@@ -0,0 +1,13 @@
+package ex1.multilayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultilayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
